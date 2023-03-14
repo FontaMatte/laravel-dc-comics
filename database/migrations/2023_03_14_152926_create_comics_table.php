d8d8da5076603d57c86e8dc2966d42dcb9386e34
FontaMatte <matteo.fontanel@gmail.com>
2023-03-14 16:50:55 +0100

    Milestone 1 completed

---
+++ database/migrations/2023_03_14_152926_create_comics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 64);
+            $table->string('description', 1064);
+            $table->string('thumb', 256);
+            $table->float('price', 4, 2)->unsigned();
+            $table->string('series', 32);
+            $table->date('sale_date');
+            $table->string('type')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
